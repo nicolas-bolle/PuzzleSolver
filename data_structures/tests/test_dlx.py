bb929a9f20e1c1e218c551c6456bfa0417cd807d
Nicolas Bolle <nicolasbolle@gmail.com>
2025-01-02 15:14:38 -0500

    finish dlx unit tests

--- data_structures/tests/test_dlx.py
+++ data_structures/tests/test_dlx.py
@@ -8,9 +8,7 @@ from data_structures.dlx import DLX, ArrayDLX
 
 
 class TestDLX(unittest.TestCase):
-    """DLX unit tests
-    The test problems will mostly be in the ArrayDLX tests since that class is easier to specify for testing
-    """
+    """DLX unit tests"""
 
     def test_basic_problem(self):
         """Basic exact cover problem"""
@@ -36,7 +34,74 @@ class TestDLX(unittest.TestCase):
         solutions_desired = {"ADG", "BDFG"}
         solutions_actual = {"".join(sorted(soln)) for soln in dlx.solutions()}
 
-        assert solutions_actual == solutions_desired
+        self.assertEqual(solutions_actual, solutions_desired)
+
+    def test_checks(self):
+        """Confirm data checks are working"""
+        # row names
+        with self.assertRaises(AssertionError) as e:
+            DLX(
+                row_names=["row", "row"],
+                col_names_primary=["col1"],
+                col_names_secondary=["col2"],
+                entries={},
+            )
+        self.assertEqual(str(e.exception), "Expected 1 count for 'row', found 2")
+
+        # column names
+        with self.assertRaises(AssertionError) as e:
+            DLX(
+                row_names=["row"],
+                col_names_primary=["col1", "col1"],
+                col_names_secondary=["col2"],
+                entries={},
+            )
+        self.assertEqual(str(e.exception), "Expected 1 count for 'col1', found 2")
+
+        with self.assertRaises(AssertionError) as e:
+            DLX(
+                row_names=["row"],
+                col_names_primary=["col1"],
+                col_names_secondary=["col2", "col2"],
+                entries={},
+            )
+        self.assertEqual(str(e.exception), "Expected 1 count for 'col2', found 2")
+
+        with self.assertRaises(AssertionError) as e:
+            DLX(
+                row_names=["row"],
+                col_names_primary=["col1"],
+                col_names_secondary=["col1"],
+                entries={},
+            )
+        self.assertEqual(str(e.exception), "Overlapping elements found such as 'col1'")
+
+        # entries
+        with self.assertRaises(AssertionError) as e:
+            DLX(
+                row_names=["row"],
+                col_names_primary=["col1"],
+                col_names_secondary=["col2"],
+                entries={"row1": ["col1"]},
+            )
+        self.assertEqual(str(e.exception), "Extra elements found such as 'row1'")
+
+        with self.assertRaises(AssertionError) as e:
+            DLX(
+                row_names=["row"],
+                col_names_primary=["col1"],
+                col_names_secondary=["col2"],
+                entries={"row": ["col3"]},
+            )
+        self.assertEqual(str(e.exception), "Extra elements found such as 'col3'")
+
+        # successful init
+        DLX(
+            row_names=["row"],
+            col_names_primary=["col1"],
+            col_names_secondary=["col2"],
+            entries={"row": ["col1"]},
+        )
 
 
 class TestArrayDLX(unittest.TestCase):
@@ -61,7 +126,7 @@ class TestArrayDLX(unittest.TestCase):
         solutions_desired = {"ADG", "BDFG"}
         solutions_actual = {"".join(sorted(soln)) for soln in dlx.solutions()}
 
-        assert solutions_actual == solutions_desired
+        self.assertEqual(solutions_actual, solutions_desired)
 
     def test_secondary_problem(self):
         """Exact problem with secondary constraints
@@ -99,10 +164,103 @@ class TestArrayDLX(unittest.TestCase):
         solutions_desired = {"AB", "CD", "CE"}
         solutions_actual = {"".join(sorted(soln)) for soln in dlx.solutions()}
 
-        assert solutions_actual == solutions_desired
+        self.assertEqual(solutions_actual, solutions_desired)
 
+    def test_checks(self):
+        """Confirm data checks are working"""
+        # array checks
+        with self.assertRaises(AssertionError) as e:
+            ArrayDLX(A=[[]], row_names=["row"])
+        self.assertEqual(str(e.exception), "A must be a np.ndarray")
 
-# TODO how default column names are set, defaults other stuff, that checks work as intended, etc.
+        with self.assertRaises(AssertionError) as e:
+            ArrayDLX(A=np.array([[]]), row_names=["row"], A_secondary=[[]])
+        self.assertEqual(str(e.exception), "A_secondary must be a np.ndarray")
+
+        with self.assertRaises(AssertionError) as e:
+            ArrayDLX(A=np.array([]), row_names=["row"])
+        self.assertEqual(str(e.exception), "A must be two dimensional")
+
+        with self.assertRaises(AssertionError) as e:
+            ArrayDLX(A=np.array([[]]), row_names=["row"], A_secondary=np.array([]))
+        self.assertEqual(str(e.exception), "A_secondary must be two dimensional")
+
+        with self.assertRaises(AssertionError) as e:
+            ArrayDLX(
+                A=np.array([[0, 0], [0, 0]]),
+                row_names=["row1", "row2"],
+                A_secondary=np.array([[0]]),
+            )
+        self.assertEqual(str(e.exception), "A has 2 rows while A_secondary has 1 rows")
+
+        # name checks
+        with self.assertRaises(AssertionError) as e:
+            ArrayDLX(
+                A=np.array([[0, 0], [0, 0]]),
+                row_names=["row"],
+                A_secondary=np.array([[0], [0]]),
+            )
+        self.assertEqual(str(e.exception), "1 rows specified, expected 2")
+
+        with self.assertRaises(AssertionError) as e:
+            ArrayDLX(
+                A=np.array([[0, 0], [0, 0]]),
+                row_names=["row1", "row2"],
+                col_names=["col"],
+                A_secondary=np.array([[0], [0]]),
+            )
+        self.assertEqual(str(e.exception), "1 columns specified, expected 2")
+
+        with self.assertRaises(AssertionError) as e:
+            ArrayDLX(
+                A=np.array([[0, 0], [0, 0]]),
+                row_names=["row1", "row2"],
+                col_names=["col1", "col2"],
+                A_secondary=np.array([[0], [0]]),
+                col_names_secondary=["col3", "col4"],
+            )
+        self.assertEqual(str(e.exception), "2 secondary columns specified, expected 1")
+
+        # successful init
+        ArrayDLX(
+            A=np.array([[0, 0], [0, 0]]),
+            row_names=["row1", "row2"],
+            col_names=["col1", "col2"],
+            A_secondary=np.array([[0], [0]]),
+            col_names_secondary=["col3"],
+        )
+
+    def test_defaults(self):
+        """Check defaults are as expected"""
+        # column names
+        dlx = ArrayDLX(
+            A=np.array([[0, 0, 0]]), A_secondary=np.array([[0, 0]]), row_names=["row"]
+        )
+        self.assertEqual(dlx.col_names, ["primary_0", "primary_1", "primary_2"])
+        self.assertEqual(dlx.col_names_secondary, ["secondary_0", "secondary_1"])
+
+        # secondary constraints
+        dlx = ArrayDLX(A=np.array([[0, 0, 0]]), row_names=["row"])
+        self.assertIsInstance(dlx.A_secondary, np.ndarray)
+        self.assertEqual(dlx.A_secondary.shape, (1, 0))
+
+    def test_translation(self):
+        """Confirm we define the the DLX fields correctly"""
+        dlx = ArrayDLX(
+            A=np.array([[1, 0], [0, 1]]),
+            row_names=["row1", "row2"],
+            col_names=["col1", "col2"],
+            A_secondary=np.array([[1], [0]]),
+            col_names_secondary=["col3"],
+        )
+
+        self.assertEqual(dlx.row_names, ["row1", "row2"])
+        self.assertEqual(dlx.col_names_primary, ["col1", "col2"])
+        self.assertEqual(dlx.col_names_secondary, ["col3"])
+
+        entries_target = {"row1": ["col1", "col3"], "row2": ["col2"]}
+        self.assertEqual(dlx.entries, entries_target)
+
 
 if __name__ == "__main__":
     unittest.main()
