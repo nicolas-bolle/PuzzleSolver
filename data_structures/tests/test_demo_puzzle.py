c4830b70c87b2285598b7696dcbe74b3591437d0
Nicolas Bolle <nicolasbolle@gmail.com>
2025-02-01 14:27:45 -0500

    update demo_puzzles unit tests

--- data_structures/tests/test_demo_puzzle.py
+++ data_structures/tests/test_demo_puzzle.py
@@ -2,17 +2,29 @@
 
 import unittest
 
-from data_structures.demo_puzzle import DemoBoard
+from data_structures.demo_puzzle import Demo1GridBoard, Demo2GridBoard, Demo3GridBoard
 
 
-class TestDemoPuzzle(unittest.TestCase):
+class TestDemoPuzzles(unittest.TestCase):
     """DemoPuzzle unit tests"""
 
-    def test(self):
+    def test_demo1(self):
         """Check we can solve the puzzle"""
-        board = DemoBoard()
+        board = Demo1GridBoard()
         solutions = list(board.solutions())
         assert len(solutions) == 8
+
+    def test_demo2(self):
+        """Check we can't solve the puzzle"""
+        board = Demo2GridBoard()
+        solutions = list(board.solutions())
+        assert len(solutions) == 0
+
+    def test_demo3(self):
+        """Check we can solve the puzzle"""
+        board = Demo3GridBoard()
+        solutions = list(board.solutions())
+        assert len(solutions) == 28
 
 
 if __name__ == "__main__":
