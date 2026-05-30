aeb232452dd6160c9023e0e09382ee26a44ac7f7
nicolas-bolle <nicolasbolle@gmail.com>
2026-05-29 23:15:11 -0400

    switch to pytest

--- puzzle-solver/data_structures/tests/test_demo_puzzle.py
+++ puzzle-solver/data_structures/tests/test_demo_puzzle.py
@@ -1,7 +1,5 @@
 """demo_puzzle.py unit tests"""
 
-import unittest
-
 from data_structures.demo_puzzle import (
     Demo1GridBoard,
     Demo2GridBoard,
@@ -11,39 +9,36 @@ from data_structures.demo_puzzle import (
 )
 
 
-class TestDemoPuzzles(unittest.TestCase):
-    """DemoPuzzle unit tests"""
-
-    def test_demo1(self):
-        """Check we can solve the puzzle"""
-        board = Demo1GridBoard()
-        solutions = list(board.solutions())
-        assert len(solutions) == 8
-
-    def test_demo2(self):
-        """Check we can't solve the puzzle"""
-        board = Demo2GridBoard()
-        solutions = list(board.solutions())
-        assert len(solutions) == 0
-
-    def test_demo3(self):
-        """Check we can solve the puzzle"""
-        board = Demo3GridBoard()
-        solutions = list(board.solutions())
-        assert len(solutions) == 28
-
-    def test_demo4(self):
-        """Check we can solve the puzzle"""
-        board = Demo4GridBoard()
-        solutions = list(board.solutions())
-        assert len(solutions) == 10
-
-    def test_demo5(self):
-        """Check we can solve the puzzle"""
-        board = Demo5GridBoard()
-        solutions = list(board.solutions())
-        assert len(solutions) == 3
+def test_demo1():
+    """Check we can solve the puzzle"""
+    board = Demo1GridBoard()
+    solutions = list(board.solutions())
+    assert len(solutions) == 8
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_demo2():
+    """Check we can't solve the puzzle"""
+    board = Demo2GridBoard()
+    solutions = list(board.solutions())
+    assert len(solutions) == 0
+
+
+def test_demo3():
+    """Check we can solve the puzzle"""
+    board = Demo3GridBoard()
+    solutions = list(board.solutions())
+    assert len(solutions) == 28
+
+
+def test_demo4():
+    """Check we can solve the puzzle"""
+    board = Demo4GridBoard()
+    solutions = list(board.solutions())
+    assert len(solutions) == 10
+
+
+def test_demo5():
+    """Check we can solve the puzzle"""
+    board = Demo5GridBoard()
+    solutions = list(board.solutions())
+    assert len(solutions) == 3
--- puzzle-solver/data_structures/tests/test_kanoodle.py
+++ puzzle-solver/data_structures/tests/test_kanoodle.py
@@ -1,18 +1,9 @@
 """demo_puzzle.py unit tests"""
 
-import unittest
-
 from data_structures.kanoodle import Kanoodle
 
 
-class TestKanoodle(unittest.TestCase):
-    """Kanoodle unit tests"""
-
-    def test_full(self):
-        """Check we can generate a solution"""
-        board = Kanoodle()
-        next(board.solutions())
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_full():
+    """Check we can generate a solution"""
+    board = Kanoodle()
+    next(board.solutions())
